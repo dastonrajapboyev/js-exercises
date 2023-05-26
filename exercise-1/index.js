eddb39e0a9951fa4fafd3d71f28cc5fc5f3dbf45
Doston <1764rajabovv@gmail.com>
2023-05-26 00:45:32 -0700

    finf object's key vhich is value a number,

---
+++ exercise-1/index.js
@@ -0,0 +1,21 @@
+// 01 find object's key 
+
+// let obj={
+//       id:1,
+//       name:"WebBrain"
+//   }
+//   for(let i in obj)
+//   console.log(i);
+// ======== completed ============
+
+// 02 finf object's key vhich is value a number
+
+let obj={
+    id:1,
+    name:"WebBrain"
+}
+
+for(values in obj){
+    console.log(obj[values]);
+}
+
